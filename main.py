fd504873955f0dd7223e3cf70c3238f88486e3f3
Antoine Gingras <antoi.gingras@gmail.com>
2025-05-17 03:39:45 -0400

    Finalisation du jeu en état fonctionnel. Ajout de la liste de mots

--- main.py
+++ main.py
@@ -1,39 +1,88 @@
 # Initialisation des constantes
+import random
 
 NBR_VIE = 6
+life = NBR_VIE
+
 answer = ""
 answer_list = ["_"] * len(answer)
+replay = "O"
+clue = False
 
-life = NBR_VIE
 
 def main():
     replay = "O"
+
     while replay == "O":
-        global
-        answer = 1
-        answer_list = ["_"] * len(answer)
+        global answer, answer_list, clue
+        answer = delete_accent(word_selection())
+        #print(answer)
+        answer_list = ["_"] * (len(answer) - 1)
         initialisation_jeu(list_to_string(answer_list))
+
         while life > 0 and check_word() == False:
+            if life == 1 and clue == False:
+                give_clue()
             guess_letter()
 
         if check_word() == True:
             print_win()
         else:
             print_lose()
-        replay = rejouer()
 
+        replay = rejouer()
+    return
+
+
+def list_to_string(word):
+    word_str = ""
+    for letters in word:
+        word_str += letters
+    return word_str
+
+
+def string_to_list(word):
+    word_list = []
+    for letters in word:
+        word_list.append(letters)
+    return word_list
+
+
+def word_selection():
+    file = "mots_pendu.txt"
+    word_list = open(file, "r", encoding='utf-8')
+    num = random.randrange(126)
+    answer = word_list.readlines()[num]
+    return answer
+
+
+def delete_accent(word_accent):
+    word = string_to_list(word_accent)
+    for i in range(len(word)):
+        if word[i] in ("à, â, ä"):
+            word[i] = "a"
+        elif word[i] in ("é","è","ê","ë"):
+            word[i] = "e"
+        elif word[i] in ("î","ï"):
+            word[i] = "i"
+        elif word[i] in ("ô", "ö"):
+            word[i] = "o"
+        elif word[i] in ("ù","û"):
+            word[i] = "u"
+    return list_to_string(word)
+
+
+def initialisation_jeu(word):
+    print(f"""\n\nBienvenue au jeu du pendu. Un mot aléatoire a été sélectionner et votre but est de le trouver.
+Dite moi une lettre et je vous dirais si elle est présente dans le mot recherché.
+Le mot contient {len(word)} lettres et vous possédez {NBR_VIE} vies.
+{list_to_string(answer_list)}\n""")
     return
 
 
 def lose_life():
     global life
     life -= 1
-    return
-
-
-def initialisation_jeu(word):
-    print(f"""\tLe mot contient {len(word)} lettres. Vous avez {NBR_VIE} chances.
-    \t{list_to_string(answer_list)}\n""")
     return
 
 
@@ -45,30 +94,17 @@ def guess_letter():
         if answer[i] == letter:
             answer_list[i] = letter
             check = True
-        if life == 1:
-            clue()
 
     if check is True:
-        print(f"""\tLa lettre {letter} est présente dans le mot recherché: {list_to_string(answer_list)}
+        print(f"""\tBien joué ! La lettre {letter} est présente dans le mot recherché: {list_to_string(answer_list)}
     Il vous reste {life} vies\n""")
+
     else:
         lose_life()
-        print(f"""\tLa lettre {letter} n'est pas présente dans le mot recherché: {list_to_string(answer_list)}
+        print(f"""\tDommage ... La lettre {letter} n'est pas présente dans le mot recherché: {list_to_string(answer_list)}
     Il vous reste {life} vies\n""")
     return answer_list
 
-
-def list_to_string(word):
-    word_str = ""
-    for letters in word:
-        word_str += letters
-    return word_str
-
-def string_to_list(word):
-    word_list = []
-    for letters in word:
-        word_list.append(letters)
-    return word_list
 
 def check_word():
     for letters in answer_list:
@@ -76,9 +112,9 @@ def check_word():
             return False
     return True
 
+
 def print_win():
-    print("""Vous avez gagné !\n
-    ======================================================================================
+    print(f"""Vous avez gagné ! Le mot rechercé était {answer}    ======================================================================================
       \\\\    //   =========   ||      ||      \\\\                //  ==========  ||\\\\     ||
        \\\\  //   ||       ||  ||      ||       \\\\              //       ||      ||  \\\\   ||
         \\\\//    ||       ||  ||      ||        \\\\    //\\\\    //        ||      ||   \\\\  ||
@@ -87,33 +123,44 @@ def print_win():
     ======================================================================================  """)
     return
 
+
 def print_lose():
-    print("""Vous avez perdu ...\n
-    ======================================================================================
+    print(f"""Vous avez perdu ... Le mot était rechercé {answer}    ======================================================================================
       \\\\    //  =========   ||      ||       ||         =========    ========   ========
        \\\\  //  ||       ||  ||      ||       ||        ||       ||  ||          ||      
         \\\\//   ||       ||  ||      ||       ||        ||       ||   ========   ||======
          ||    ||       ||  ||      ||       ||        ||       ||          ||  ||      
          ||     =========    ========         =======   ========     ========   ========
-    ======================================================================================  """)
+    ======================================================================================""")
     return
 
-def clue():
-    user_input = ""
-    while user_input not in ("O", "N"):
-        user_input = input("Voulez-vous un indice ? (O/N)")
-        if user_input not in ("O", "N"):
-            print("Veuillez répondre par O pour oui ou N pour Non")
-    if user_input = "O":
 
+def give_clue():
+    user_input = ""
+    global answer, clue
+    while user_input not in ("O", "N"):
+        user_input = input("Voulez-vous un indice ? (O/N): ")
+        if user_input not in ("O", "N"):
+            print("Veuillez répondre par O pour oui ou N pour Non\n")
+    if user_input == "O":
+        index = answer_list.index("_", random.randrange(len(answer_list)))
+        answer_list[index] = answer[index]
+        print(f"Le mot contient la lettre {answer[index]} ! "
+              f"Le mot ressemble à ceci : {list_to_string(answer_list)} !")
+        clue = True
         return
+
 
 def rejouer():
     user_input = ""
+    global clue, life
+    life = 6
+    clue = False
     while user_input not in ("O", "N"):
-        user_input = input("Voulez-vous rejouer ? (O/N)")
+        user_input = input("Voulez-vous rejouer ? (O/N): ")
         if user_input not in ("O", "N"):
-            print("Veuillez répondre par O pour oui ou N pour Non")
+            print("Veuillez répondre par O pour oui ou N pour Non\n")
     return user_input
+
 
 main()
